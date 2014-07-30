3f8953ea3347d06d60dc006accfcb84f866503b5
malikov <malikovincent@gmail.com>
2014-07-30 00:49:23 -0400

    minor change

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -18,7 +18,7 @@ auth.oauthCallback = function(req,res){
 
 	passport.authenticate(params.type,function(err, user, info) {
 	    if (err) {
-	    	return res.status(400).json({payload : {error: info}, message : "An error occured passport.authenticate"});
+	    	return res.status(400).json({payload : {error: info}, message : "An erro occured passport.authenticate"});
 	 	}
 
 	    if (!user) { 
